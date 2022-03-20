7f10d1e632cb910611619ffde2ed407f3045545f
Scolopendria <probablyEukaryotic@gmail.com>
2022-03-20 10:02:33 -0700

    child support full

--- src/Classes.h
+++ src/Classes.h
@@ -5,27 +5,28 @@
 #include <vector>
 #include <array>
 class verStr{
-    //child support, destruct/back-translate and command interface support still needed
+    //destruct/back-translate and command interface support still needed
     public:
         verStr(std:: string s);
         ~verStr(){};
         std::string getString();
         std::string getName();
         std::string format();
-        std::vector<std::string> listChildren();
-        std::vector<std::string> listAttribute();
-        std::string getValue(std::string attrName);
-        verStr* child(std::string childName);
-        void setAttribute(std::string attrName, std::string attrValue);
+        std::vector<std::string> listChildren();                        //
+        std::vector<std::string> listAttribute();                       //
+        std::string getValue(std::string attrName);                     //
+        verStr* child(std::string childName);                           //
+        void setAttribute(std::string attrName, std::string attrValue); //
         verStr* createChildren(std::string childName);
         verStr* createAttribute(std::string identifier[2]);
-        void deleteChildren(std::string childName);
-        void deleteAttribute(std::string attrName);
+        void deleteChildren(std::string childName);                     //
+        void deleteAttribute(std::string attrName);                     //
     private:
         std::string read(std::string s, std::string::size_type &i);
+        std::string cut(std::string::size_type i[2]);
         verStr* strip();
         verStr* objectify();
-        verStr* translate();
+        verStr* translate();                                            //
         std::string String;
         std::string Name;
         std::vector<verStr> children;
--- src/verStr.cpp
+++ src/verStr.cpp
@@ -56,13 +56,19 @@ std::string verStr::format(){
     return pStr;
 }
 
-verStr* verStr::createAttribute(std::string identifier[2]){
-    attribute.push_back({identifier[0], identifier[1]});
+
+verStr* verStr::createChildren(std::string childName){
+    children.push_back(verStr{childName});
+    return this;
+}
+
+verStr* verStr::createAttribute(std::string ID_pair[2]){
+    attribute.push_back({ID_pair[0], ID_pair[1]});
     return this;
 }
 
 std::string verStr::read(std::string s, std::string::size_type &i){
-    // if (String[0] != '"'){ // Possible redundant guard since all input will be filtered through strip()
+    // if (String[0] != '"'){ // Guard deactivated beacuse all input will be purified through strip
     //     std::cerr << "Missing symbol: (\") at position 0: ***"
     //         << String << "***" << std::endl;
     //     exit(EXIT_FAILURE);
@@ -71,7 +77,17 @@ std::string verStr::read(std::string s, std::string::size_type &i){
     i++;
     while (i < s.length() && s[i] != '"') identifier += s[i++];
     i++;
+    if (i == String.length()){
+        std::cerr << "verStr::read(): File Overran" << std::endl;
+        exit(EXIT_FAILURE);
+    }
     return identifier;
+}
+
+std::string verStr::cut(std::string::size_type i[2]){
+    std::string pStr;
+    while (i[0] < i[1]) pStr += String[i[0]++];
+    return pStr;
 }
 
 verStr* verStr::strip(){
@@ -108,23 +124,54 @@ verStr* verStr::strip(){
 }
 
 verStr* verStr::objectify(){
-    std::string pStr[2]{"", ""};
+    std::string ID_pair[2];
+    std::string pStr;
+    std::string::size_type i{0}, pincer[2];
+    int depth;
     char p;
-    std::string::size_type i{0};
-    int depth{0};
     Name = read(String, i);
     i ++;
     
-    while (i < String.length()){
-        pStr[0] = read(String, i);
+    while (i < (String.length() - 1)){
+        pincer[0] = i;
+        ID_pair[0] = read(String, i);
         p = String[i++];
         if (p == '='){
-            pStr[1] = read(String, i);
-            createAttribute(pStr);
+            ID_pair[1] = read(String, i);
+            createAttribute(ID_pair);
+        } else if (p == '{'){
+            depth = 1;
+            while (i < String.length() && depth != 0){
+                switch (String[i]){
+                    case '"': read(String, i);
+                    break;
+                    case '{': depth++; i++;
+                    break;
+                    case '}': depth--; i++;
+                    break;
+                    case '=': i++;
+                    break;
+                    default: std::cerr << "Unexpected Character: ("<< String[i] << ")" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+            if (depth == 0){
+                pincer[1] = i;
+                createChildren(cut(pincer));
+            } else if (i == String.length()){
+                std::cerr << "verStr::objectify(): File Overran" << std::endl;
+                exit(EXIT_FAILURE);
+            } else {
+                std::cerr << "verStr::objectify(): Unknown Error" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            std::cerr << "Unexpected character: (" << p << ")" << std::endl;
+            exit(EXIT_FAILURE);
         }
-        // Add child support
     }
 
+    std::cout << Name << std::endl;
     for (auto &ID_pair : attribute){
         for (auto &s: ID_pair){
             std::cout << s << " ";
